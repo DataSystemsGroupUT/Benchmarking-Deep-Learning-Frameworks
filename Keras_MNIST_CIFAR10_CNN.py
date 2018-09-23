862f3fc6d5aebc768fce155b336150167abb8331
Yousef Essam <yousef.essam.eldin@gmail.com>
2018-09-23 17:03:24 +0300

    Keras Modular Code Mnist and Cifar10

--- Keras_MNIST_CIFAR10_CNN.py
+++ Keras_MNIST_CIFAR10_CNN.py
@@ -10,29 +10,52 @@ Created on Sun Sep  2 15:33:56 2018
 from __future__ import print_function
 import keras
 from keras.datasets import cifar10,mnist
-from keras.models import Sequential,optimizers
+from keras import Sequential,optimizers
 from keras.layers import Conv2D, MaxPooling2D, Dense, Dropout, Activation, Flatten
 import os
 import psutil
 import time
 import datetime
 
-Dataset = cifar10
-MNIST_batch_size = 128
-MNIST_num_classes = 10
-MNIST_epochs = 12
-# input image dimensions
-MNIST_img_rows, MNIST_img_cols = 28, 28
 
 
-CIFAR_batch_size = 32
-CIFAR_num_classes = 10
-CIFAR_epochs = 12
-CIFAR_save_dir = os.path.join(os.getcwd(), 'saved_models')
-CIFAR_model_name = 'keras_cifar10_trained_model.h5'
+
+
+def initParameters(dataset,batchSize,numClasses,epochs):
+    global Dataset    
+    global pbatchSize
+    global pnumClasses
+    global pEpochs
+    
+    Dataset = dataset
+    pbatchSize = batchSize
+    pnumClasses = numClasses
+    pEpochs = epochs
+    
         
+    
+
 def loadData():
     (x_train, y_train), (x_test, y_test) = Dataset.load_data()
+    global imgRows
+    global imgCols
+    global inputShape
+    imgRows = x_train.shape[1]
+    imgCols = x_train.shape[2]
+
+    try:
+        imgRGB_Dimensions = x_train.shape[3]
+    except Exception:
+        imgRGB_Dimensions = 1 #For Gray Scale Images
+
+    print(x_train.shape)
+    x_train = x_train.reshape(x_train.shape[0], imgRows, imgCols, imgRGB_Dimensions)
+    x_test = x_test.reshape(x_test.shape[0], imgRows, imgCols, imgRGB_Dimensions)
+    x_train = x_train.astype('float32')
+    x_test = x_test.astype('float32')
+    x_train, x_test = NormalizeData(x_train, x_test)
+    y_train, y_test = CategorizeData(y_train,y_test,pnumClasses)
+    inputShape = (imgRows, imgCols, imgRGB_Dimensions)
     return x_train, y_train, x_test, y_test
 
 def NormalizeData(x_train,x_test):
@@ -42,20 +65,20 @@ def NormalizeData(x_train,x_test):
     
 # convert class vectors to binary class matrices
 # The result is a vector with a length equal to the number of categories.
-def CategorizeData(y_train,y_test,num_classes):
-    y_train = keras.utils.to_categorical(y_train, num_classes)
-    y_test = keras.utils.to_categorical(y_test, num_classes)
+def CategorizeData(y_train,y_test,pnumClasses):
+    y_train = keras.utils.to_categorical(y_train, pnumClasses)
+    y_test = keras.utils.to_categorical(y_test, pnumClasses)
     return y_train, y_test
 
 def StartMonitoring():
         process = psutil.Process(os.getpid())
         print("Before memory_percent", process.memory_percent())
         print(psutil.cpu_percent(percpu=True))
+        global start
         start = time.time()
-        return start
     
     
-def EndMonitoring(start):
+def EndMonitoring():
         end = time.time()        
         process = psutil.Process(os.getpid())
         print("after memory_percent",process.memory_percent())
@@ -68,57 +91,54 @@ def EndMonitoring(start):
     
 def RunMNIST():
         x_train, y_train, x_test, y_test = loadData()
-        x_train = x_train.reshape(x_train.shape[0], MNIST_img_rows, MNIST_img_cols, 1)
-        x_test = x_test.reshape(x_test.shape[0], MNIST_img_rows, MNIST_img_cols, 1)
-        x_train = x_train.astype('float32')
-        x_test = x_test.astype('float32')
-        MNIST_input_shape = (MNIST_img_rows, MNIST_img_cols, 1)
-         # normalizing the data to help with the training
-        x_train, x_test = NormalizeData(x_train, x_test)
-        y_train, y_test = CategorizeData(y_train,y_test,MNIST_num_classes)
-                 
+ 
         # building model layers with the sequential model
         MNIST_model = Sequential()
-        MNIST_model.add(Conv2D(32, kernel_size=(3, 3),
-                         activation='relu',
-                         input_shape=MNIST_input_shape))
+        
+        MNIST_model.add(Conv2D(32, kernel_size=(3, 3),activation='relu',input_shape=inputShape))
         MNIST_model.add(Conv2D(64, (3, 3), activation='relu'))
         MNIST_model.add(MaxPooling2D(pool_size=(2, 2)))
         MNIST_model.add(Dropout(0.25))
         MNIST_model.add(Flatten())
+        
         MNIST_model.add(Dense(128, activation='relu'))
         MNIST_model.add(Dropout(0.5))
-        MNIST_model.add(Dense(MNIST_num_classes, activation='softmax'))
+        MNIST_model.add(Dense(pnumClasses, activation='softmax'))
         
         MNIST_sgd = optimizers.SGD(lr=0.01, decay=1e-6, momentum=0.5, nesterov=True)
         MNIST_model.compile(loss='categorical_crossentropy',
                       optimizer=MNIST_sgd,
                       metrics=['accuracy'])
     
-        start = StartMonitoring()
+        StartMonitoring()
         #Training the model
         MNIST_model.fit(x_train, y_train,
-                  batch_size=MNIST_batch_size,
-                  epochs=MNIST_epochs,
+                  batch_size=pbatchSize,
+                  epochs=pEpochs,
                   verbose=1,
                   validation_data=(x_test, y_test))
         
-        EndMonitoring(start)
+        EndMonitoring()
+        
         #Model performance evaluation
-        MNIST_Test_loss , MNIST_acc = MNIST_model.evaluate(x_test, y_test, verbose=1)
-        print("Test Loss", MNIST_Test_loss)
-        print("Test Accuracy", MNIST_acc)
+        evaluateModel(MNIST_model,x_test, y_test, verbose=1)
+       
         #MNIST_predicted_classes = MNIST_model.predict_classes(MNIST_x_test)
 
+
+
+def evaluateModel(model,x_test,y_test):
+     pLoss, pAcc = model.evaluate(x_test, y_test, verbose=1)
+     print("Test Loss", pLoss)
+     print("Test Accuracy", pAcc)
+
 def RunCIFAR10():
+        
         x_train, y_train, x_test, y_test = loadData()
-        x_train = x_train.astype('float32')
-        x_test = x_test.astype('float32')
-        x_train, x_test = NormalizeData(x_train, x_test)
-        y_train, y_test = CategorizeData(y_train,y_test,CIFAR_num_classes)
+ 
         CIFAR_model = Sequential()
-        CIFAR_model.add(Conv2D(32, (3, 3), padding='same',
-                         input_shape=x_train.shape[1:]))
+        
+        CIFAR_model.add(Conv2D(32, (3, 3), padding='same',input_shape=inputShape))
         CIFAR_model.add(Activation('relu'))
         CIFAR_model.add(Conv2D(32, (3, 3)))
         CIFAR_model.add(Activation('relu'))
@@ -131,49 +151,43 @@ def RunCIFAR10():
         CIFAR_model.add(Activation('relu'))
         CIFAR_model.add(MaxPooling2D(pool_size=(2, 2)))
         CIFAR_model.add(Dropout(0.25))
-        
         CIFAR_model.add(Flatten())
         CIFAR_model.add(Dense(512))
         CIFAR_model.add(Activation('relu'))
-        CIFAR_model.add(Dropout(0.5))
-        CIFAR_model.add(Dense(CIFAR_num_classes))
-        CIFAR_model.add(Activation('softmax')) 
-        CIFAR_model.compile(loss='categorical_crossentropy',
-                      optimizer='adam',
-                      metrics=['accuracy'])
         
-        start = StartMonitoring()
+        CIFAR_model.add(Dropout(0.5))
+        CIFAR_model.add(Dense(pnumClasses))
+        CIFAR_model.add(Activation('softmax')) 
+        
+        CIFAR_model.compile(loss='categorical_crossentropy', optimizer='adam',metrics=['accuracy'])
+        
+        StartMonitoring()
         
         CIFAR_model.fit(x_train, y_train,
-                      batch_size=CIFAR_batch_size,
-                      epochs=CIFAR_epochs,
+                      batch_size=pbatchSize,
+                      epochs=pEpochs,
                       validation_data=(x_test, y_test),
                       shuffle=True)
-        EndMonitoring(start)
-        # Save the model
-        if not os.path.isdir(CIFAR_save_dir):
-            os.makedirs(CIFAR_save_dir)
-        CIFAR_model_path = os.path.join(CIFAR_save_dir, CIFAR_model_name)
-        CIFAR_model.save(CIFAR_model_path)
-        print('Saved trained model at %s ' % CIFAR_model_path)
+        
+        EndMonitoring()
         
         # Score trained model.
-        CIFAR_scores = CIFAR_model.evaluate(x_test, y_test, verbose=1)
-        print('Test loss:', CIFAR_scores[0])
-        print('Test accuracy:', CIFAR_scores[1])
+        evaluateModel(CIFAR_model,x_test, y_test, verbose=1)
+       
         
-def main():
+def runModel(dataset,batchSize=128,numClasses=10,epochs=12):
+    initParameters(dataset,batchSize,numClasses,epochs)
     if Dataset is mnist:
         RunMNIST()
     elif Dataset is cifar10:
         RunCIFAR10()     
     else:
         print("Choose cifar10 or mnist")
+
+def main():
+    runModel(mnist,epochs=1)
+    #runModel(cifar10,epochs=1)
+  
 if __name__ == '__main__':
         main()
-        
-        
-        
-        
-        
-        
+       

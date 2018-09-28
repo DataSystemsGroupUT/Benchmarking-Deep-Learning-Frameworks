f6f8fb65b4d6dc4ee67f811f1b66e0f485fdad67
Yousef Essam <yousef.essam.eldin@gmail.com>
2018-09-28 13:59:42 +0300

    MXNET Done

--- MXNet_CIFAR_MNIST_CNN.py
+++ MXNet_CIFAR_MNIST_CNN.py
@@ -40,21 +40,21 @@ def NormalizeData(x_train,x_test):
     
     # convert class vectors to binary class matrices
     # The result is a vector with a length equal to the number of categories.
-def CategorizeData(y_train,y_test,pnumClasses):
-    print(y_train)
-    print(y_train.shape)    
-    b = np.zeros((y_train.shape[0],pnumClasses))
-    b[np.arange(y_train.shape[0]),y_train] = 1
-    y_train = b
-    print(y_train)
-    
-    b = np.zeros((y_test.shape[0],pnumClasses))
-    b[np.arange(y_test.shape[0]),y_test] = 1
-    y_test = b
-    
-    print(y_train)
-    print(y_test)
-    return y_train, y_test 
+#def CategorizeData(y_train,y_test,pnumClasses):
+#    print(y_train)
+#    print(y_train.shape)    
+#    b = np.zeros((y_train.shape[0],pnumClasses))
+#    b[np.arange(y_train.shape[0]),y_train] = 1
+#    y_train = b
+#    print(y_train)
+#    
+#    b = np.zeros((y_test.shape[0],pnumClasses))
+#    b[np.arange(y_test.shape[0]),y_test] = 1
+#    y_test = b
+#    
+#    print(y_train)
+#    print(y_test)
+#    return y_train, y_test 
 
 def transform(data, label):
     return nd.transpose(data.astype(np.float32), (2,0,1))/255, label.astype(np.float32)
@@ -68,10 +68,14 @@ def loadData():
         train_data = gluon.data.DataLoader(gluon.data.vision.CIFAR10(train=True, transform=transform),pbatchSize, shuffle=True)
         test_data = gluon.data.DataLoader(gluon.data.vision.CIFAR10(train=False, transform=transform),pbatchSize, shuffle=False)
         return train_data, test_data
+    elif(Dataset ==  "cifar100"):
+        train_data = gluon.data.DataLoader(gluon.data.vision.CIFAR100(train=True, transform=transform),pbatchSize, shuffle=True)
+        test_data = gluon.data.DataLoader(gluon.data.vision.CIFAR100(train=False, transform=transform),pbatchSize, shuffle=False)
+        return train_data, test_data
     else:
         pass
 
-def loadDataSVHC(extra=False):
+def loadDataSVHC(fname,extra=False):
     """Load the SVHN dataset (optionally with extra images)
     Args:
         extra (bool, optional): load extra training data
@@ -87,7 +91,6 @@ def loadDataSVHC(extra=False):
     data = uYN.Dataset()
     data.classes = np.arange(10)
 
-    fname = 'C:/Users/Youssef/Downloads/Compressed/%s_32x32.mat'
 
     X, y = load_mat(fname % 'train')
     data.train_images = X
@@ -116,27 +119,22 @@ def loadDataSVHC(extra=False):
     except Exception:
         imgRGB_Dimensions = 1 #For Gray Scale Images
 
-    print(x_train.shape)
     
     x_train = x_train.reshape(x_train.shape[0], imgRows, imgCols, imgRGB_Dimensions)
     x_test = x_test.reshape(x_test.shape[0], imgRows, imgCols, imgRGB_Dimensions)
     x_train = x_train.astype('float32')
     x_test = x_test.astype('float32')
     x_train, x_test = NormalizeData(x_train, x_test)
-    y_train, y_test = CategorizeData(y_train,y_test,10)
     inputShape = (imgRows, imgCols, imgRGB_Dimensions)
     
-    print(x_train.shape)
-    print(y_train.shape)
     
     x_train = x_train.reshape(len(y_train), imgRows, imgCols, imgRGB_Dimensions)
-    
     x_train = nd.array(x_train, dtype=x_train.dtype)
     label = nd.array(y_train,dtype=y_train.dtype)
     
     x_test = x_test.reshape(len(y_test), imgRows, imgCols, imgRGB_Dimensions)
     x_test = nd.array(x_test, dtype=x_test.dtype)
-    labelTest = y_test.astype(np.int32)
+    labelTest = nd.array(y_test,dtype=y_test.dtype)
     return x_train,label, x_test,labelTest
 
 
@@ -174,59 +172,89 @@ def modelCIFAR10():
         #softmax takes 10
     return  net
 
+def modelCIFAR100():
+    net = gluon.nn.Sequential()
+    with net.name_scope():
+        net.add(gluon.nn.Conv2D(channels=128, kernel_size=3,strides=1,padding=1, activation='relu'))
+        net.add(gluon.nn.Conv2D(channels=128, kernel_size=3,padding=1, activation='relu'))
+        net.add(gluon.nn.MaxPool2D(pool_size=2, strides=2))
+        net.add(gluon.nn.Dropout(0.1))
+        
+        net.add(gluon.nn.Conv2D(channels=256, kernel_size=3, activation='relu'))
+        net.add(gluon.nn.Conv2D(channels=256, kernel_size=3, activation='relu'))
+        net.add(gluon.nn.MaxPool2D(pool_size=2, strides=2))
+        net.add(gluon.nn.Dropout(0.25))
+        
+        
+        net.add(gluon.nn.Conv2D(channels=512, kernel_size=3, activation='relu'))
+        net.add(gluon.nn.Conv2D(channels=512, kernel_size=3, activation='relu'))
+        net.add(gluon.nn.MaxPool2D(pool_size=2, strides=2))
+        net.add(gluon.nn.Dropout(0.5))
+        
+        
+        net.add(gluon.nn.Flatten())
+        net.add(gluon.nn.Dense(1024, activation="relu"))
+        net.add(gluon.nn.Dropout(0.5))
+        net.add(gluon.nn.Dense(pnumClasses * 10))
+        #softmax takes 10
+    return  net
+
 def model_SVHC():
     
     model_SVHC = gluon.nn.Sequential()
     with model_SVHC.name_scope():
-        model_SVHC.add(gluon.nn.Conv2D(channels=48, kernel_size=5, activation='relu'))
+        model_SVHC.add(gluon.nn.Conv2D(channels=48, kernel_size=5,strides=1,padding=2, activation='relu'))
         model_SVHC.add(gluon.nn.MaxPool2D(pool_size=2, strides=2))
         model_SVHC.add(gluon.nn.Dropout(0.2))
    
     
-        model_SVHC.add(gluon.nn.Conv2D(channels=64, kernel_size=5))
+        model_SVHC.add(gluon.nn.Conv2D(channels=64, kernel_size=5,strides=1,padding=2))
         model_SVHC.add(gluon.nn.BatchNorm(axis=1, center=True, scale=True))
         model_SVHC.add(gluon.nn.Activation(activation='relu'))
         model_SVHC.add(gluon.nn.MaxPool2D(pool_size=2, strides=1))
         model_SVHC.add(gluon.nn.Dropout(0.2))
         
-        model_SVHC.add(gluon.nn.Conv2D(channels=128, kernel_size=5))
+        model_SVHC.add(gluon.nn.Conv2D(channels=128, kernel_size=5,strides=1,padding=2))
         model_SVHC.add(gluon.nn.BatchNorm(axis=1, center=True, scale=True))
         model_SVHC.add(gluon.nn.Activation(activation='relu'))
         model_SVHC.add(gluon.nn.MaxPool2D(pool_size=2, strides=2))
         model_SVHC.add(gluon.nn.Dropout(0.2))
             
-        model_SVHC.add(gluon.nn.Conv2D(channels=160, kernel_size=5))
+        model_SVHC.add(gluon.nn.Conv2D(channels=160, kernel_size=5,strides=1,padding=2))
         model_SVHC.add(gluon.nn.BatchNorm(axis=1, center=True, scale=True))
         model_SVHC.add(gluon.nn.Activation(activation='relu'))
         model_SVHC.add(gluon.nn.MaxPool2D(pool_size=2, strides=1))
         model_SVHC.add(gluon.nn.Dropout(0.2))
         
-        model_SVHC.add(gluon.nn.Conv2D(channels=192, kernel_size=5))
+        model_SVHC.add(gluon.nn.Conv2D(channels=192, kernel_size=5,strides=1,padding=2))
         model_SVHC.add(gluon.nn.BatchNorm(axis=1, center=True, scale=True))
         model_SVHC.add(gluon.nn.Activation(activation='relu'))
         model_SVHC.add(gluon.nn.MaxPool2D(pool_size=2, strides=2))
         model_SVHC.add(gluon.nn.Dropout(0.2))
         
-        model_SVHC.add(gluon.nn.Conv2D(channels=192, kernel_size=5))
+        model_SVHC.add(gluon.nn.Conv2D(channels=192, kernel_size=5,strides=1,padding=2))
         model_SVHC.add(gluon.nn.BatchNorm(axis=1, center=True, scale=True))
         model_SVHC.add(gluon.nn.Activation(activation='relu'))
         model_SVHC.add(gluon.nn.MaxPool2D(pool_size=2, strides=1))
         model_SVHC.add(gluon.nn.Dropout(0.2))
         
-        model_SVHC.add(gluon.nn.Conv2D(channels=192, kernel_size=5))
+        model_SVHC.add(gluon.nn.Conv2D(channels=192, kernel_size=3,strides=1,padding=2))
         model_SVHC.add(gluon.nn.BatchNorm(axis=1, center=True, scale=True))
         model_SVHC.add(gluon.nn.Activation(activation='relu'))
         model_SVHC.add(gluon.nn.MaxPool2D(pool_size=2, strides=2))
         model_SVHC.add(gluon.nn.Dropout(0.2))
         
-        model_SVHC.add(gluon.nn.Conv2D(channels=192, kernel_size=5))
+        model_SVHC.add(gluon.nn.Conv2D(channels=192, kernel_size=5,strides=1,padding=2))
         model_SVHC.add(gluon.nn.BatchNorm(axis=1, center=True, scale=True))
         model_SVHC.add(gluon.nn.Activation(activation='relu'))
         model_SVHC.add(gluon.nn.MaxPool2D(pool_size=2, strides=1))
         model_SVHC.add(gluon.nn.Dropout(0.2))
+        
+        model_SVHC.add(gluon.nn.Flatten())
+        model_SVHC.add(gluon.nn.Dense(3072, activation="relu"))
+        model_SVHC.add(gluon.nn.Dense(3072, activation="relu"))
+      
         model_SVHC.add(gluon.nn.Dense(pnumClasses))
-
-     
     return model_SVHC        
 
 def evaluate_accuracy(data_iterator, net):
@@ -239,7 +267,24 @@ def evaluate_accuracy(data_iterator, net):
         acc.update(preds=predictions, labels=label)
     return acc.get()[1]
 
-
+def evaluate_accuracySVHC(X,Y,batchSize, net):
+    acc = mx.metric.Accuracy()
+    i =0 
+    print(X.shape,Y.shape,batchSize)
+    for data, label in zip(batch(X, batchSize),batch(Y, batchSize)):
+        data = data.as_in_context(myDevice)
+        label = label.as_in_context(myDevice)
+        data = np.transpose(data,(0,3,1,2))
+        output = net(data)
+        predictions = nd.argmax(output, axis=1)
+        acc.update(preds=predictions, labels=label)
+        i=i+1
+    return acc.get()[1]
+        
+def batch(iterable, n=1):
+    l = len(iterable)
+    for ndx in range(0, l, n):
+        yield iterable[ndx:min(ndx + n, l)]
 
 def RunMNIST(dataset,batchSize,numClasses,epochs,learningRate,momentum,weightDecay):
     
@@ -259,12 +304,10 @@ def RunMNIST(dataset,batchSize,numClasses,epochs,learningRate,momentum,weightDec
     #Optimizer settings
     trainer = gluon.Trainer(net.collect_params(), 'sgd', {'learning_rate': learningRate, 'momentum': momentum,'wd': weightDecay})
     
-    for e in range(pEpochs+1):
+    print("MNIST Training Started.....")
+    memT,cpuT = YN.StartLogger("MXNET","MNIST")
+    for e in range(pEpochs):
         for i, (data, label) in list(enumerate(train_data)):
-            print(str(i) + "*******")
-            print(data.shape)
-            print(label.shape)
-            print("*******")
             data = data.as_in_context(myDevice)
             label = label.as_in_context(myDevice)
             with autograd.record():
@@ -273,11 +316,12 @@ def RunMNIST(dataset,batchSize,numClasses,epochs,learningRate,momentum,weightDec
             loss.backward()
             trainer.step(data.shape[0])
             curr_loss = nd.mean(loss).asscalar()
-            #moving_loss = (curr_loss if ((i == 0) and (e == 0)) else (1 - 0.01) * moving_loss + 0.01 * curr_loss)
-        
+        print("Calculating metrics.....")
         test_accuracy = evaluate_accuracy(test_data, net)
         train_accuracy = evaluate_accuracy(train_data, net)
         print("Epoch %s. Loss: %s, Train_acc %s, Test_acc %s" % (e, curr_loss, train_accuracy, test_accuracy))
+    print("MNIST Training Finished.....")
+    memT,cpuT = YN.StartLogger("MXNET","MNIST")
         
 def RunCIFAR10(dataset,batchSize,numClasses,epochs,learningRate,momentum,weightDecay):
     
@@ -295,8 +339,9 @@ def RunCIFAR10(dataset,batchSize,numClasses,epochs,learningRate,momentum,weightD
     
     #Optimizer settings
     trainer = gluon.Trainer(net.collect_params(), 'sgd', {'learning_rate': learningRate, 'momentum': momentum,'wd': weightDecay})
-    
-    for e in range(pEpochs+1):
+    print("CIFAR10 Training Started.....")
+    memT,cpuT = YN.StartLogger("MXNET","CIFAR10")
+    for e in range(pEpochs):
         for i, (data, label) in enumerate(train_data):
             data = data.as_in_context(myDevice)
             label = label.as_in_context(myDevice)
@@ -306,26 +351,54 @@ def RunCIFAR10(dataset,batchSize,numClasses,epochs,learningRate,momentum,weightD
             loss.backward()
             trainer.step(data.shape[0])
             curr_loss = nd.mean(loss).asscalar()
-            #moving_loss = (curr_loss if ((i == 0) and (e == 0)) else (1 - 0.01) * moving_loss + 0.01 * curr_loss)
-        
         test_accuracy = evaluate_accuracy(test_data, net)
         train_accuracy = evaluate_accuracy(train_data, net)
-        print("Epoch %s. Loss: %s, Train_acc %s, Test_acc %s" % (e, curr_loss, train_accuracy, test_accuracy))
+        print("Epoch %s. Loss: %s, Train_acc %s, Test_acc %s" % (e+1, curr_loss, train_accuracy, test_accuracy))
+    print("CIFAR Training Finished.....")
+    memT,cpuT = YN.StartLogger("MXNET","CIFAR10")
 
 
-
-def RunSVHC(dataset,batchSize,numClasses,epochs,learningRate,momentum,weightDecay):
+def RunCIFAR100(dataset,batchSize,numClasses,epochs,learningRate,momentum,weightDecay):
     
     initParameters(dataset,batchSize,numClasses,epochs,learningRate,momentum,weightDecay)
     
-    train_data_x,train_data_y, test_data_x,test_data_y = loadDataSVHC()
-    print("**********")
-    print(enumerate(train_data_x))
-    print("**********")
+    train_data, test_data = loadData()
+    
     #we didn’t have to include the softmax layer 
     #because MXNet’s has an efficient function that simultaneously computes 
     #the softmax activation and cross-entropy loss
     
+    #Get Model
+    net = modelCIFAR100()
+    net.collect_params().initialize(mx.init.Xavier(magnitude=2.24), ctx=myDevice)
+    
+    #Optimizer settings
+    trainer = gluon.Trainer(net.collect_params(), 'sgd', {'learning_rate': learningRate, 'momentum': momentum,'wd': weightDecay})
+    print("CIFAR100 Training Started.....")
+    memT,cpuT = YN.StartLogger("MXNET","CIFAR100")
+    for e in range(pEpochs):
+        for i, (data, label) in enumerate(train_data):
+            print("\r........ " + str(i) + " ........" )
+            data = data.as_in_context(myDevice)
+            label = label.as_in_context(myDevice)
+            with autograd.record():
+                output = net(data)
+                loss = gluon.loss.SoftmaxCrossEntropyLoss()(output, label)
+            loss.backward()
+            trainer.step(data.shape[0])
+            curr_loss = nd.mean(loss).asscalar()
+        test_accuracy = evaluate_accuracy(test_data, net)
+        train_accuracy = evaluate_accuracy(train_data, net)
+        print("Epoch %s. Loss: %s, Train_acc %s, Test_acc %s" % (e+1, curr_loss, train_accuracy, test_accuracy))
+    print("CIFAR Training Finished.....")
+    memT,cpuT = YN.StartLogger("MXNET","CIFAR100")
+
+        
+def RunSVHC(dataset,batchSize,numClasses,epochs,learningRate,momentum,weightDecay,fname):
+    
+    initParameters(dataset,batchSize,numClasses,epochs,learningRate,momentum,weightDecay)
+    train_data_x,train_data_y, test_data_x,test_data_y = loadDataSVHC(fname)
+   
     #Get Model
     net = model_SVHC()
     net.collect_params().initialize(mx.init.Xavier(magnitude=2.24), ctx=myDevice)
@@ -333,43 +406,42 @@ def RunSVHC(dataset,batchSize,numClasses,epochs,learningRate,momentum,weightDeca
     #Optimizer settings
     trainer = gluon.Trainer(net.collect_params(), 'sgd', {'learning_rate': learningRate, 'momentum': momentum,'wd': weightDecay})
     
-    dataXList = []
-    dataYList = []
-    for e in range(pEpochs+1):
-        for i, (data,label) in list(enumerate(zip(train_data_x,train_data_y))):
-            #print(i)
-            #print(data)
-            #print(label)
-            dataXList.append(data)
-            dataYList.append(label)
-            if(i == batchSize):
-                break
-            
-        #for i, (data,label) in list(enumerate(zip(train_data_x,train_data_y))):
-        dataXList = dataXList.as_in_context(myDevice)
-        print(dataXList.shape)
-        dataYList = dataYList.as_in_context(myDevice)
-        with autograd.record():
-            #data = data.reshape(1,data.shape[0],data.shape[1],data.shape[2])
-            #print(data.shape)
-            output = net(dataXList)
-            loss = gluon.loss.SoftmaxCrossEntropyLoss()(output, dataYList)
-        loss.backward()
-        trainer.step(dataYList.shape[0])
-        curr_loss = nd.mean(loss).asscalar()
-            #moving_loss = (curr_loss if ((i == 0) and (e == 0)) else (1 - 0.01) * moving_loss + 0.01 * curr_loss)
-        
-        test_accuracy = evaluate_accuracy(test_data, net)
-        train_accuracy = evaluate_accuracy(train_data, net)
-        print("Epoch %s. Loss: %s, Train_acc %s, Test_acc %s" % (e, curr_loss, train_accuracy, test_accuracy))     
+
+    print("SVHC Training Started.....")
+    memT,cpuT = YN.StartLogger("MXNET","SVHC")
+    for e in range(pEpochs):
+        i=0
+        for data, label in zip(batch(train_data_x, batchSize),batch(train_data_y, batchSize)):
+            #print("Batch: " + str(i+1))
+            data = data.as_in_context(myDevice)
+            label = label.as_in_context(myDevice)
+            data = np.transpose(data,(0,3,1,2))
+            with autograd.record():
+                output = net(data)
+                loss = gluon.loss.SoftmaxCrossEntropyLoss()(output, label)
+            loss.backward()
+            trainer.step(data.shape[0])
+            curr_loss = nd.mean(loss).asscalar()
+            i=i+1
+        print("Calculating metrics.....")
+        #print("Yousef ---> ",train_data_x.shape,train_data_y.shape,batchSize)
+        train_accuracy = evaluate_accuracySVHC(train_data_x,train_data_y,batchSize, net)
+        test_accuracy = evaluate_accuracySVHC(test_data_x,test_data_y,batchSize, net)
+        print("Epoch %s. Loss: %s, Train_acc %s, Test_acc %s" % (e+1, curr_loss, train_accuracy, test_accuracy))     
+    print("SVHC Training Finished.....")
+    memT,cpuT = YN.StartLogger("MXNET","SVHC")
 
 def runModel(dataset,batchSize=128,numClasses=10,epochs=12,learningRate=0.01,momentum=0.5,weightDecay=1e-6):
     if dataset is "mnist":
         RunMNIST(dataset,batchSize,numClasses,epochs,learningRate,momentum,weightDecay)
     elif dataset is "cifar10":
         RunCIFAR10(dataset,batchSize,numClasses,epochs,learningRate,momentum,weightDecay)
+    elif dataset is "cifar100":
+        RunCIFAR100(dataset,batchSize,numClasses,epochs,learningRate,momentum,weightDecay)
     elif dataset is "SVHC":
-        RunSVHC(dataset,batchSize,numClasses,epochs,learningRate,momentum,weightDecay) 
+        #fname = "./%s_32x32.mat"
+        fname = 'C:/Users/Youssef/Downloads/Compressed/%s_32x32.mat'
+        RunSVHC(dataset,batchSize,numClasses,epochs,learningRate,momentum,weightDecay,fname) 
     else:
         print("Choose cifar10 or mnist")
 
@@ -377,7 +449,8 @@ def main():
     
     #runModel("mnist",epochs=1)
     #runModel("cifar10",epochs=1)
-    runModel("SVHC",epochs=1)
+    runModel("SVHC",epochs=15)
+    #runModel("cifar100",epochs=3)
     
     
   

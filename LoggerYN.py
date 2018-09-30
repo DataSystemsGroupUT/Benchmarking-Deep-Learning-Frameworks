2cd37287d6fb1a93ba2e644e894fd916b925d8d1
Yousef Essam <yousef.essam.eldin@gmail.com>
2018-09-30 18:24:55 +0300

    modification1

--- LoggerYN.py
+++ LoggerYN.py
@@ -7,12 +7,19 @@ Created on Tue Sep 25 20:08:11 2018
 import subprocess
 import os
 def StartLogger(frameworkName,datasetName):
-    print(os.getpid())
+    #print(os.getpid())
     cPid = os.getpid()
     command_memory ="python memLog.py "+ str(cPid) + " " + str(frameworkName) + " " + str(datasetName) + " "
     command_cpu ="python cpuLog.py "+ str(cPid) + " " + str(frameworkName) + " " + str(datasetName) + " "
-    memory_task = subprocess.Popen(command_memory, stdout=subprocess.PIPE, shell=True)
-    cpu_task = subprocess.Popen(command_cpu, stdout=subprocess.PIPE, shell=True)
+    memory_task = ""
+    cpu_task = ""
+    try:
+        memory_task = subprocess.Popen(command_memory, stdout=subprocess.PIPE, shell=True)
+        cpu_task = subprocess.Popen(command_cpu, stdout=subprocess.PIPE, shell=True)
+
+    except:
+      print("Error while running the logger service")
+    
     return memory_task,cpu_task
 
 def EndLogger(memory_task,cpu_task):
